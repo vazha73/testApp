b1605a61c2de659958d785d2aad48c6ff3562ea4
GeorgeSikharulidze <g.sikharulidze2001@gmail.com>
2022-01-23 01:16:01 +0400

    giorgi calculator

--- src/app/api/calculation/george.py
+++ src/app/api/calculation/george.py
@@ -1,2 +1,16 @@
 from fastapi import APIRouter
 router = APIRouter()
+
+@router.get("/giorgi/calculator/")
+async def read_root(a: int, b: int, op: str):
+    # using anonymous functions to define operators
+    operators = {"plus": (lambda a,b: a+b), "minus": (lambda a,b: a-b), "multiply": (lambda a,b: a*b), "divide": (lambda a,b: a/b)}
+    if op not in operators.keys():
+        return {"error": "operator unknown. use (plus, minus, multiply, divide)."}
+
+    try:
+        return operators[op] (a, b)
+    except ZeroDivisionError:
+        return {"error": "can't divide by 0."}
+    except Exception:
+        return {"error": "unknown error."}
