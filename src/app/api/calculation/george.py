838455363136a507547467f12fe09d048be28ac9
GeorgeSikharulidze <g.sikharulidze2001@gmail.com>
2022-01-24 01:49:12 +0400

    improved my calculator

--- src/app/api/calculation/george.py
+++ src/app/api/calculation/george.py
@@ -1,16 +1,52 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, validator, ValidationError
 router = APIRouter()
 
-@router.get("/giorgi/calculator/")
-async def read_root(a: int, b: int, op: str):
-    # using anonymous functions to define operators
-    operators = {"plus": (lambda a,b: a+b), "minus": (lambda a,b: a-b), "multiply": (lambda a,b: a*b), "divide": (lambda a,b: a/b)}
-    if op not in operators.keys():
-        return {"error": "operator unknown. use (plus, minus, multiply, divide)."}
+class Calculator(BaseModel):
+    a: float
+    b: float
+    op: str
 
-    try:
-        return operators[op] (a, b)
-    except ZeroDivisionError:
-        return {"error": "can't divide by 0."}
-    except Exception:
-        return {"error": "unknown error."}
+    @validator("op")
+    def is_valid_operator(cls, op, values):
+        operators = ("plus", "minus", "multiply", "divide")
+        if op not in operators:
+            raise ValueError("invalid operator. must be (plus, minus, multiply, divide)")
+        elif values["b"] == 0 and op == "divide":
+            raise ValueError("can't divide by 0")
+        return op        
+
+class Calculate:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def solution(self, op):
+        if op == "plus":
+            return self.a + self.b
+        elif op == "minus":
+            return self.a - self.b
+        elif op == "multiply":
+            return self.a * self.b
+        elif op == "divide":
+            return self.a / self.b
+
+@router.post("/giorgi/calculator/")
+async def read_root(calculator: Calculator):
+    answer = Calculate(calculator.a, calculator.b)
+    return answer.solution(calculator.op)
+
+
+# @router.post("/giorgi/calculator/")
+# async def read_root(a: int, b: int, op:str):
+#     # using anonymous functions to define operators
+#     operators = {"plus": (lambda a,b: a+b), "minus": (lambda a,b: a-b), "multiply": (lambda a,b: a*b), "divide": (lambda a,b: a/b)}
+#     if op not in operators.keys():
+#         return {"error": "operator unknown. use (plus, minus, multiply, divide)"}
+
+#     try:
+#         return operators[op] (a, b)
+#     except ZeroDivisionError:
+#         return {"error": "can't divide by 0"}
+#     except Exception:
+#         return {"error": "unknown error"}
